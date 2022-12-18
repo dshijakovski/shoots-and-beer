1b23576362afd262adfe44a9c60935899c77364d
dshijakovskiTDL <daniel.shijakovski@testdevlab.com>
2022-12-18 15:31:14 +0100

    new game, high scores activities added

--- app/src/main/java/com/example/shotsandbeer/MainActivity.java
+++ app/src/main/java/com/example/shotsandbeer/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.shotsandbeer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button newGameButton = findViewById(R.id.newGameBtn);
+        newGameButton.setOnClickListener(v -> {
+            startActivity(new Intent(this, NewGame.class));
+        });
+
+        Button highScoresButton = findViewById(R.id.highScoresBtn);
+        highScoresButton.setOnClickListener(v -> {
+            startActivity(new Intent(this, HighScores.class));
+        });
     }
 }
